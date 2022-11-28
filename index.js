5806ff07aecfdf901d2dc71b5defa45a89460d69
jobokcoder <zjaqld@naver.com>
2022-11-28 22:59:42 +0900

    feat: UX/UI 구성

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const member = new Member();
-const memberButton = document.querySelector('.members__button');
-const memberList   = document.querySelector('.members__list');
-const printButton = document.querySelector(".print__button");
-const printList   = document.querySelector(".print__list");
+const memberButton = document.querySelector('.ord__lottery--append-btn');
+const radnomButton = document.querySelector('.ord__lottery--random-btn');
 
 function printMember() {
     const members = member.list();
-    memberList.innerHTML = '';
+    const wrapper = document.querySelector('.ord__lottery--players');
+    const name = document.querySelector('.ord__lottery--append-input')
+
+    wrapper.textContent = '';
+    name.value = '';
+
     members.forEach(item => {
         const dom = createDom('div')
-        const deleteButton = createDom('button', 'members__bel-button')
-        deleteButton.innerText = '삭제';
-        deleteButton.onclick = () => {
+        dom.onclick = () => {
             member.remove(item);
             printMember();
         }
-        dom.append(item, deleteButton);
-        memberList.append(dom);
+        dom.append(item);
+        wrapper.append(dom);
     })
 }
 
 memberButton.addEventListener('click', function() {
-    const name = document.querySelector('.members__input').value;
+    const name = document.querySelector('.ord__lottery--append-input').value;
 
     if(name !== "") {
         member.add(name);
@@ -30,12 +31,32 @@ memberButton.addEventListener('click', function() {
     printMember();
 })
 
-printButton.addEventListener('click', function() {
+radnomButton.addEventListener('click', function() {
     const members = member.list();
-    printList.innerText = '';
+    const wrapper = document.querySelector('.ord__lottery--list');
     members.forEach(member => {
-        const dom = document.createElement('div');
-        dom.innerText = `${member}의 상위는 [${randomHighUnit()}]`;
-        printList.append(dom);
+        const item = document.createElement('div');
+        const player = document.createElement('span');
+        const thumbnail = document.createElement('div');
+        const img = document.createElement('img');
+        const name = document.createElement('span');
+        const type = document.createElement('span');
+
+        item.classList.add('ord__lottery--item');
+        player.classList.add('ord__lottery--player');
+        thumbnail.classList.add('ord__lottery--profile');
+        name.classList.add('ord__lottery--name');
+        type.classList.add('ord__lottery--type');
+
+        player.textContent = member;
+        name.textContent = randomHighUnit();
+        img.alt = randomHighUnit();
+        img.src = '#';
+        type.textContent ='등급';
+        type.classList.add('im'); // css 참고
+
+        item.append(player, thumbnail, name, type);
+        thumbnail.append(img);
+        wrapper.append(item);
     })
 })
