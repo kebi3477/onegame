225cffc45611dc94a52ac8ed0d87cd87c84deb57
jobokcoder <zjaqld@naver.com>
2022-12-03 22:33:28 +0900

    feat: 유닛 데이터 공격타입 추가

--- object/unit.js
+++ object/unit.js
@@ -4,82 +4,108 @@ const unitArray = [
         value: [
             {
                 name: "루피",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/189.png?id=20221101215431"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/189.png?id=20221101215431",
+                type: "물리"
             },{
                 name: "조로",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/191.png?id=20220912110146"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/191.png?id=20220912110146",
+                type: "물리"
             },{
                 name: "나미",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/192.png?id=20221001141104"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/192.png?id=20221001141104",
+                type: "마법"
             },{
                 name: "우솝",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/195.png?id=20220619204549"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/195.png?id=20220619204549",
+                type: "물리"
             },{
                 name: "상디",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/190.png?id=20220928024246"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/190.png?id=20220928024246",
+                type: "마법"
             },{
                 name: "쵸파",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/196.png?id=20220916012020"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/196.png?id=20220916012020",
+                type: "물리"
             },{
                 name: "로빈",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/197.png?id=20221101215034"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/197.png?id=20221101215034",
+                type: "물리"
             },{
                 name: "프랑키",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/198.png?id=20220917101901"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/198.png?id=20220917101901",
+                type: "마법"
             },{
                 name: "브룩",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/199.png?id=20220912095505"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/199.png?id=20220912095505",
+                type: "마법"
             },{
                 name: "샹크스",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/205.png?id=20220820002226"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/205.png?id=20220820002226",
+                type: "마법"
             },{
                 name: "검은수염",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/204.png?id=20220809223034"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/204.png?id=20220809223034",
+                type: "마법"
             },{
                 name: "시라호시",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/200.png?id=20221119230346"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/200.png?id=20221119230346",
+                type: "마법"
             },{
                 name: "아오키지",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/202.png?id=20220813224507"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/202.png?id=20220813224507",
+                type: "마법"
             },{
                 name: "아카이누",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/201.png?id=20220912100534"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/201.png?id=20220912100534",
+                type: "마법"
             },{
                 name: "키자루",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/203.png?id=20221101215859"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/203.png?id=20221101215859",
+                type: "마법"
             },{
                 name: "로우",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/206.png?id=20220912094024"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/206.png?id=20220912094024",
+                type: "마법"
             },{
                 name: "도플라밍고",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/208.png?id=20221111232255"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/208.png?id=20221111232255",
+                type: "물리"
             },{
                 name: "사보",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/216.png?id=20220912102619"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/216.png?id=20220912102619",
+                type: "물리"
             },{
                 name: "후지토라",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/207.png?id=20221101221037"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/207.png?id=20221101221037",
+                type: "물리"
             },{
                 name: "타시기",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/209.png?id=20221003220553"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/209.png?id=20221003220553",
+                type: "마법"
             },{
                 name: "루치",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/250.png?id=20220911233422"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/250.png?id=20220911233422",
+                type: "마법"
             },{
                 name: "바질 호킨스",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/270.png?id=20220912122801"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/270.png?id=20220912122801",
+                type: "물리"
             },{
                 name: "징베",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/272.png?id=20221101220600"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/272.png?id=20221101220600",
+                type: "물리"
             },{
                 name: "스네이크맨",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/273.png?id=20221001141717"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/273.png?id=20221001141717",
+                type: "마법"
             },{
                 name: "키드",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/292.png?id=20220917095427"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/292.png?id=20220917095427",
+                type: "마법"
             },{
                 name: "야마토",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/299.png?id=20221026233755"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/299.png?id=20221026233755",
+                type: "물리"
             },
         ]
     }, {
@@ -87,37 +113,48 @@ const unitArray = [
         value: [
             {
                 name: "골 D. 로져",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/177.png?id=20221010200150"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/177.png?id=20221010200150",
+                type: "물리"
             },{
                 name: "실버즈 레일리",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/176.png?id=20220916013748"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/176.png?id=20220916013748",
+                type: "물리"
             },{
                 name: "스코퍼 가반",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/178.png?id=20220130010224"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/178.png?id=20220130010224",
+                type: "물리"
             },{
                 name: "에드워드 뉴게이트                ",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/180.png?id=20221002235158"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/180.png?id=20221002235158",
+                type: "물리"
             },{
                 name: "몽키 D. 가프",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/182.png?id=20221101221747"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/182.png?id=20221101221747",
+                type: "물리"
             },{
                 name: "센고쿠",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/179.png?id=20221101221517"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/179.png?id=20221101221517",
+                type: "마법"
             },{
                 name: "시키",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/175.png?id=20220809222532"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/175.png?id=20220809222532",
+                type: "마법"
             },{
                 name: "몽키 D. 드래곤",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/183.png?id=20221003221019"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/183.png?id=20221003221019",
+                type: "마법"
             },{
                 name: "제파",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/181.png?id=20220919215654"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/181.png?id=20220919215654",
+                type: "마법"
             },{
                 name: "카이도",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/257.png?id=20221001144025"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/257.png?id=20221001144025",
+                type: "물리"
             },{
                 name: "샬롯 링링",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/211.png?id=20221002031922"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/211.png?id=20221002031922",
+                type: "마법"
             }
         ]
     }, {
@@ -125,25 +162,32 @@ const unitArray = [
         value: [
             {
                 name: "에이스",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/189.png?id=20221101215431"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/189.png?id=20221101215431",
+                type: "마법"
             },{
                 name: "핸콕",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/222.png?id=20220913005711"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/222.png?id=20220913005711",
+                type: "마법"
             },{
                 name: "카번딧슈",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/220.png?id=20221010200031"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/220.png?id=20221010200031",
+                type: "물리"
             },{
                 name: "버기",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/219.png?id=20220511002304"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/219.png?id=20220511002304",
+                type: "물리"
             },{
                 name: "비비",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/218.png?id=20220130005847"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/218.png?id=20220130005847",
+                type: "마법"
             },{
                 name: "미호크",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/221.png?id=20220912135609"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/221.png?id=20220912135609",
+                type: "마법"
             },{
                 name: "오뎅",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/276.png?id=20220912114418"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/276.png?id=20220912114418",
+                type: "마법"
             },
         ]
     }, {
@@ -151,28 +195,36 @@ const unitArray = [
         value: [
             {
                 name: "에넬",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/215.png?id=20220912101012"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/215.png?id=20220912101012",
+                type: "마법"
             }, {
                 name: "크로커다일",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/187.png?id=20220912112953"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/187.png?id=20220912112953",
+                type: "물리"
             }, {
                 name: "레베카",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/193.png?id=20221101214655"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/193.png?id=20221101214655",
+                type: "물리"
             }, {
                 name: "아인",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/142.png?id=20221002205839"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/142.png?id=20221002205839",
+                type: "마법"
             }, {
                 name: "샬롯 카타쿠리",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/251.png?id=20220324213135"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/251.png?id=20220324213135",
+                type: "물리"
             }, {
                 name: "시노부",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/269.png?id=20220916014325"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/269.png?id=20220916014325",
+                type: "마법"
             }, {
                 name: "킹",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/291.png?id=20220912103259"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/291.png?id=20220912103259",
+                type: "물리"
             }, {
                 name: "레드필드",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/214.png?id=20220919213749"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/214.png?id=20220919213749",
+                type: "마법"
             },
         ]
     },     {
@@ -180,31 +232,40 @@ const unitArray = [
         value: [
             {
                 name: "키쿄우",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/279.png?id=20221001142908"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/279.png?id=20221001142908",
+                type: "마법"
             }, {
                 name: "브로냐·자이칙",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/282.png?id=20220403230438"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/282.png?id=20220403230438",
+                type: "마법"
             }, {
                 name: "쿠죠 죠타로",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/285.png?id=20221107204144"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/285.png?id=20221107204144",
+                type: "물리"
             }, {
                 name: "나미카제 미나토",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/283.png?id=20220805211642"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/283.png?id=20220805211642",
+                type: "마법"
             }, {
                 name: "료우기 시키",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/284.png?id=20220411002014"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/284.png?id=20220411002014",
+                type: "물리"
             }, {
                 name: "쿠치키 뱌쿠야",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/298.png?id=20220411003127"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/298.png?id=20220411003127",
+                type: "마법"
             }, {
                 name: "전율의 타츠마키",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/300.png?id=20220429234344"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/300.png?id=20220429234344",
+                type: "물리"
             }, {
                 name: "마운틴.D.히그마",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/288.png?id=20220403230431"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/288.png?id=20220403230431",
+                type: "마법"
             }, {
                 name: "야쿠모 유카리",
-                image: "https://ordsearch.speedgabia.com/images/units/ord/289.png?id=20220403230444"
+                image: "https://ordsearch.speedgabia.com/images/units/ord/289.png?id=20220403230444",
+                type: "마법"
             }
         ]
     }
